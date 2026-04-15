c42302235f673b51446d90c0e3fb99bb58fecfb0
ToanTN <tnt728595@gmail.com>
2026-04-15 09:11:55 +0700

    Add logic for login and middleware

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const prisma = require("../prisma/client");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
     try {
@@ -45,7 +46,49 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
 
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Missing email or password"
+            });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {email}
+        });
+
+        if(!user || password != user.password){
+            return res.status(401).json({
+                message: "Incorrect login information"
+            });
+        }
+
+        const token = jwt.sign({
+            sub: user.id,
+            email: user.email,
+            name: user.fullName,
+            role: user.role
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: "1h"});
+
+        //Bo password ra khoi user truoc khi tra ve cho FE
+        const {password: pass, ...userWithoutPassword} = user;
+
+        return res.status(200).json({
+            message: "Login successfully",
+            token,
+            user: userWithoutPassword
+        });
+    }
+    catch(error){
+        console.error("Login error: ", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
 };
 
 module.exports = {
