1ff2a25642803f4410dc92eb4042819d181a03c7
ToanTN <tnt728595@gmail.com>
2026-04-24 21:48:21 +0700

    Fix some BE errors

--- server/src/prisma/client.js
+++ server/src/prisma/client.js
@@ -1,5 +1,12 @@
+const {createPool} = require("mariadb");
+const {PrismaMariaDb} = require("@prisma/adapter-mariadb");
 const {PrismaClient} = require("@prisma/client");
 
-const prisma = new PrismaClient;
+const pool = createPool({
+    uri: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaMariaDb(pool);
+const prisma = new PrismaClient({adapter});
 
 module.exports = prisma;
--- server/src/server.js
+++ server/src/server.js
@@ -19,3 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 route(app);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...");
+    console.log("http://localhost:3000");
+});
