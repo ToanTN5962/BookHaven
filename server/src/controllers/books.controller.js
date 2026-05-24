5cb4fb3c565795017b1e3b88c5da057c14e30c9a
ToanTN <tnt728595@gmail.com>
2026-05-24 10:07:37 +0700

    Update BE for getting hottest books

--- server/src/controllers/books.controller.js
+++ server/src/controllers/books.controller.js
@@ -4,8 +4,14 @@ const getRandomBooks = async (req, res) => {
         const maxResults = 10;
         const startIndex = (page - 1) * maxResults;
 
+        // const response = await fetch(
+        //     `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=relevance&maxResults=${maxResults}&startIndex=${startIndex}&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+        // );
+
+        const randomLetter = "abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 26)];
+
         const response = await fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=relevance&maxResults=${maxResults}&startIndex=${startIndex}&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+            `https://www.googleapis.com/books/v1/volumes?q=${randomLetter}&maxResults=${maxResults}&startIndex=${Math.floor(Math.random() * 100)}&key=${process.env.GOOGLE_BOOKS_API_KEY}`
         );
 
         const data = await response.json();
@@ -28,7 +34,7 @@ const getRandomBooks = async (req, res) => {
         return res.status(200).json({ books, totalPages: 10 });
     } catch (error) {
         //console.error("Lỗi books controller:", error);
-        return res.status(500).json({ message: "Lỗi server", error: error.message });
+        return res.status(500).json({ message: "Server error", error: error.message });
     }
 };
 
@@ -66,7 +72,48 @@ const getBookById = async (req, res) => {
     
 };
 
+const getTopRated = async(req, res) => {
+    const nytRes = await fetch(
+        `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${process.env.NYT_API_KEY}`
+    );
+    const { results } = await nytRes.json();
+
+    //Lay cuốn sách hot nhất của mỗi thể loại
+    const topBooks = results.lists.map(list => ({
+        genre: list.list_name,
+        book: list.books[0] 
+    }));
+
+    const topBooksDetails = await Promise.all(
+        topBooks.map(async ({ genre, book }) => {
+            const isbn13 = book.isbns[0]?.isbn13;
+
+            const res = await fetch(
+                `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn13}&key=${GOOGLE_BOOKS_API_KEY}`
+            );
+            const data = await res.json();
+            const googleBook = data.items?.[0]?.volumeInfo;
+
+            return {
+                genre,
+                title: book.title,
+                author: book.author,
+                cover: book.book_image,
+                isbn13: book.primary_isbn13,
+                rating: googleBook?.averageRating,
+                ratingsCount: googleBook?.ratingsCount,
+                pageCount: googleBook?.pageCount,
+                publishedDate: googleBook?.publishedDate,
+                categories: googleBook?.categories,
+            };
+        })
+    );  
+
+    return topBooksDetails;
+};
+
 module.exports = { 
     getRandomBooks,
-    getBookById
+    getBookById,
+    getTopRated
 };
--- server/src/routes/books.route.js
+++ server/src/routes/books.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getRandomBooks, getBookById } = require("../controllers/books.controller");
+const { getRandomBooks, getBookById, getTopRated } = require("../controllers/books.controller");
 
 router.get("/random", getRandomBooks);
 router.get("/:bookId", getBookById);
+router.get("/toprate", getTopRated);
 
 module.exports = router;
